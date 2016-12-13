ee5ebf199e3a7d498a6a5077cd2bf5691e87a237
罄天 <qinliang.ql@alibaba-inc.com>
2016-12-13 19:35:12 +0800

    添加注释

--- src/mixin.js
+++ src/mixin.js
@@ -2,33 +2,44 @@ import { getOwnPropertyDescriptors, getOwnKeys } from './private/utils';
 
 const { defineProperty, getPrototypeOf } = Object;
 
+//只有Symbol对象会返回true
+//Object.prototype.toString.call(Symbol());
 function buggySymbol(symbol) {
   return Object.prototype.toString.call(symbol) === '[object Symbol]' && typeof(symbol) === 'object';
 }
 
+//调用方式：hasProperty(key, target.prototype)
 function hasProperty(prop, obj) {
   // We have to traverse manually prototypes' chain for polyfilled ES6 Symbols
   // like "in" operator does.
+  //对于ES6的Symbol类型，我们必须手动去遍历原型链，就像in操作符一样
   // I.e.: Babel 5 Symbol polyfill stores every created symbol in Object.prototype.
   // That's why we cannot use construction like "prop in obj" to check, if needed
   // prop actually exists in given object/prototypes' chain.
   if (buggySymbol(prop)) {
     do {
       if (obj === Object.prototype) {
+        // Object.prototype具有很多方法，如defineProperty等函数
         // Polyfill assigns undefined as value for stored symbol key.
         // We can assume in this special case if there is nothing assigned it doesn't exist.
+        //如果当前的Object原型对象的某个属性不是undefined，那么表示当前原型对于有这个属性
         return typeof(obj[prop]) !== 'undefined';
       }
+      //如果当前原型对象有这个属性，那么返回true
       if (obj.hasOwnProperty(prop)) {
         return true;
       }
     } while (obj = getPrototypeOf(obj));
+    //继续遍历上一个原型对象
+
     return false;
   } else {
+    //如果不是Symble类型，那么我们直接使用in操作符来完成
     return prop in obj;
   }
 }
 
+//如果第一个参数就是函数，那么其实就是啥也没有做
 function handleClass(target, mixins) {
   if (!mixins.length) {
     throw new SyntaxError(`@mixin() class ${target.name} requires at least one mixin as an argument`);
@@ -36,11 +47,24 @@ function handleClass(target, mixins) {
 
   for (let i = 0, l = mixins.length; i < l; i++) {
     const descs = getOwnPropertyDescriptors(mixins[i]);
+    /*
+    const Theme = {
+      setTheme() {}
+    };
+    这个对象通过getOwnPropertyDescriptors获取到的对象如下：
+     setTheme:{
+        configurable:true
+        enumerable:true
+        value:()
+        writable:true
+     }
+        */
+    //获取的是mixin中每一个元素的属性描述符
     const keys = getOwnKeys(descs);
-
+    //得到所有的key值，如setTheme等
     for (let j = 0, k = keys.length; j < k; j++) {
       const key = keys[j];
-
+       //如果目标的《原型上》没有这个key，那么我们才会使用definePropery来完成
       if (!(hasProperty(key, target.prototype))) {
         defineProperty(target.prototype, key, descs[key]);
       }
@@ -49,9 +73,25 @@ function handleClass(target, mixins) {
 }
 
 export default function mixin(...mixins) {
+  //第一个参数是否是函数，如果是那么返回handleClass
   if (typeof mixins[0] === 'function') {
     return handleClass(mixins[0], []);
   } else {
+    //如果不是函数，比如是对象,这时候允许你再传入一个对象作为target。如下例：
+    /*
+    import React, { Component } from 'React';
+    import { mixin } from 'core-decorators';
+    const PureRender = {
+      shouldComponentUpdate() {}
+    };
+    const Theme = {
+      setTheme() {}
+    };
+    @mixin(PureRender, Theme)
+    class MyComponent extends Component {
+      render() {}
+    }
+    */
     return target => {
       return handleClass(target, mixins);
     };
--- src/private/utils.js
+++ src/private/utils.js
@@ -3,14 +3,32 @@ import lazyInitialize from '../lazy-initialize';
 const { defineProperty, getOwnPropertyDescriptor,
         getOwnPropertyNames, getOwnPropertySymbols } = Object;
 
+//return an array of all symbol properties found directly upon the given object.
+//getOwnPropertySymbols：返回一个指定对象具有的所有的Symbol属性，其返回值为一个数组
+//而Object.getOwnPropertyNames()不会返回Symbol属性，只会返回字符串属性
+//如果一个对象没有手动设置过Symbol属性，那么他会返回一个空数组，请看下例：
+/*
+  var obj = {};
+  var a = Symbol('a');
+  var b = Symbol.for('b');
+  obj[a] = 'localSymbol';
+  obj[b] = 'globalSymbol';
+  var objectSymbols = Object.getOwnPropertySymbols(obj);
+  console.log(objectSymbols.length); // 2
+  console.log(objectSymbols);        // [Symbol(a), Symbol(b)]
+  console.log(objectSymbols[0]);     // Symbol(a)
+*/
+
+
+//因为Descriptor对象也是一个对象，所以最后肯定会继承Object，所以肯定会有hasOwnPropery属性
 export function isDescriptor(desc) {
   if (!desc || !desc.hasOwnProperty) {
     return false;
   }
-
   const keys = ['value', 'initializer', 'get', 'set'];
 
   for (let i = 0, l = keys.length; i < l; i++) {
+    //如果该对象有只有有一个属性，value/initializer/get/set那么他就是描述符，否则为false
     if (desc.hasOwnProperty(keys[i])) {
       return true;
     }
@@ -20,6 +38,7 @@ export function isDescriptor(desc) {
 }
 
 export function decorate(handleDescriptor, entryArgs) {
+  //如果entryArgs是一个数组，同时数组最后一个元素也是Descriptor对象
   if (isDescriptor(entryArgs[entryArgs.length - 1])) {
     return handleDescriptor(...entryArgs, []);
   } else {
@@ -53,6 +72,8 @@ export function metaFor(obj) {
   return obj[META_KEY];
 }
 
+//(1)如果有getOwnPropertySymbols，那么直接返回getOwnPropertySymbols和getOwnPropertyNames的组合
+//否则直接调用getOwnPropertyNames
 export const getOwnKeys = getOwnPropertySymbols
     ? function (object) {
         return getOwnPropertyNames(object)
@@ -61,9 +82,10 @@ export const getOwnKeys = getOwnPropertySymbols
     : getOwnPropertyNames;
 
 
+
+//首先获取该对象具有的所有的key集合，然后把Descriptor对象赋值到一个对象上并返回
 export function getOwnPropertyDescriptors(obj) {
   const descs = {};
-
   getOwnKeys(obj).forEach(
     key => (descs[key] = getOwnPropertyDescriptor(obj, key))
   );
@@ -71,6 +93,7 @@ export function getOwnPropertyDescriptors(obj) {
   return descs;
 }
 
+//这里是创建一个默认的setter方法，其底层是调用defineProperty来完成的
 export function createDefaultSetter(key) {
   return function set(newValue) {
     Object.defineProperty(this, key, {
